1a491b2b05af4f32c9dbfcd17032ee04ebfd0695
Karol Bulwin <karolbulwin@gmail.com>
2019-03-08 17:13:09 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+    },
+    "rules": {
+        "comma-dangle": ["error", "never"],
+        "no-console": "off",
+    }
 };
