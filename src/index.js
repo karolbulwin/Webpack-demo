7f3b064237a74a5d41d579bdfb2bf0e2775f3d7a
Karol Bulwin <karolbulwin@gmail.com>
2019-03-08 23:17:04 +0100

    Cleaning up the /dist folder

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import _ from 'lodash';
 // import './style.css';
 // import Icon from './icon.jpg';
 // import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   // Add the image to our existing div.
 
