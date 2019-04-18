d3ad87b6f05d66e1b3c14b214fe2bfa042c960bc
Karol Bulwin <karolbulwin@gmail.com>
2019-04-18 18:29:13 +0200

    add lazy loading, plus some cleaning

--- src/index.js
+++ src/index.js
@@ -1,39 +1,19 @@
-import _ from 'lodash';
 import '../public/stylesheets/styles.scss';
+import mainComponent from './main.js';
 import headerComponent from './header.js';
-import { cube } from './math.js';
+import footerComponent from './footer.js';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-  const pre = document.createElement('pre');
-  // const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  pre.innerHTML = [
-    'Hello webpack!',
-    `5 cubed is equal to ${cube(5)}`
-  ].join('\n\n');
-
-  btn.innerHTML = 'Click me and check the console!!';
-  btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-    const print = module.default;
-    print();
-  })
-
-  var io = new IntersectionObserver(
+function observer(element) {
+  const io = new IntersectionObserver(
     entries => {
-      console.log(entries);
-      console.log(entries[0].isIntersecting)
       if (entries[0].isIntersecting) {
-        import('./ele');
+        import('./img');
+        import('./loadButton');
       }
-
-      // import('./ele');
     },
     {
       /* Using default options. Details below */
@@ -41,12 +21,11 @@ function component() {
   );
   // Start observing an element
   io.observe(element);
-  console.log(io);
-  
-  element.appendChild(btn);
-
-  return element;
 }
-document.body.appendChild(headerComponent());
 
-document.body.appendChild(component());
+document.body.appendChild(headerComponent());
+document.body.appendChild(mainComponent());
+
+observer(document.querySelector('.img-container'));
+
+document.body.appendChild(footerComponent());
